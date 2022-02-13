312365cb9334f95a556cb8a356ee3c67a0c68da5
Spencer Dent <sd00070@my.westga.edu>
2022-02-13 05:36:15 -0500

    remove erroneous mailer config
    > I realized I did not add the config properly,
    > but as the testing and dev environments are
    > using :test, configuring the delivery_method
    > for all environments would be counter-
    > productive.

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Configure email in all environments.
-config.action_mailer.delivery_method = :smtp
