3b9a7a69e9c2f2fe5510f227feb63cf3cc42f624
Spencer Dent <sd00070@my.westga.edu>
2022-02-13 04:32:05 -0500

    make method private

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -65,18 +65,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  def pay_type_params
-    if order_params[:pay_type] == "Credit card"
-      params.require(:order).permit(:credit_card_number, :expiration_date)
-    elsif order_params[:pay_type] == "Check"
-      params.require(:order).permit(:routing_number, :account_number)
-    elsif order_params[:pay_type] == "Purchase order"
-      params.require(:order).permit(:po_number)
-    else
-      {}
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
@@ -91,6 +79,18 @@ class OrdersController < ApplicationController
     def ensure_cart_isnt_empty
       if @cart.line_items.empty?
         redirect_to store_index_url, notice: 'Your cart is empty'
+      end
+    end
+
+    def pay_type_params
+      if order_params[:pay_type] == "Credit card"
+        params.require(:order).permit(:credit_card_number, :expiration_date)
+      elsif order_params[:pay_type] == "Check"
+        params.require(:order).permit(:routing_number, :account_number)
+      elsif order_params[:pay_type] == "Purchase order"
+        params.require(:order).permit(:po_number)
+      else
+        {}
       end
     end
 end
