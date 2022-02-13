004d92e1ca6906c6b3238fa56c6415d823519a6f
Spencer Dent <sd00070@my.westga.edu>
2022-02-13 05:20:25 -0500

    config all environments to send email via smtp

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configure email in all environments.
+config.action_mailer.delivery_method = :smtp
