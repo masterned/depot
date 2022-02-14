f0588cd31fe3506b9439b89ebe1d53d3d8b7c8a2
Spencer Dent <sd00070@my.westga.edu>
2022-02-14 02:51:16 -0500

    re-add accidentally removed routing# input

--- app/javascript/PayTypeSelector/CheckPayType.jsx
+++ app/javascript/PayTypeSelector/CheckPayType.jsx
@@ -8,7 +8,12 @@ class CheckPayType extends React.Component {
           <label htmlFor="order_routing_number">
             {I18n.t("orders.form.check_pay_type.routing_number")}
           </label>
+
+          <input type="password"
+            name="order[routing_number]"
+            id="order_routing_number" />
         </div>
+
         <div className="field">
           <label htmlFor="order_account_number">
             {I18n.t("orders.form.check_pay_type.account_number")}
